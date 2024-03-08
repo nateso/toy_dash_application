b28af7e9a06e09fd1b49300a23d2f5744e78f79c
Schmidt-Ott <nathanael.schmidt-ott@giz.de>
2024-03-08 18:28:22 +0100

    change debug mode

--- app.py
+++ app.py
@@ -133,6 +133,7 @@ app.callback(
 # ------------------------- run the dashboard ---------------------------------------------------------------------------
 if __name__ == '__main__':
     #app.run_server(host="0.0.0.0")
-    app.run_server(debug = True)
-    #app.run_server()
+    #app.run_server(debug = True)
+    app.run_server()
+
 
